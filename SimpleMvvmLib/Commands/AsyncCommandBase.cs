03b4d2b083187c042e00cfa50aa8ec35660319a5
3k-dome <doenerfresser3k@gmail.com>
2022-08-02 13:14:16 +0200

    added async command base

---
+++ SimpleMvvmLib/Commands/AsyncCommandBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleMvvmLib.Commands {
+
+    public abstract class AsyncCommandBase : ICommand {
+
+        readonly Action<Exception>? _onException;
+
+        private bool _isExecuting;
+        public bool IsExecuting {
+            get { return this._isExecuting; }
+            set {
+                this._isExecuting = value;
+                this.CanExecuteChanged?.Invoke(this.CanExecuteChanged, new());
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public AsyncCommandBase(Action<Exception>? onException = null) {
+            this._onException = onException;
+        }
+
+        public virtual bool CanExecute(object parameter) => !this.IsExecuting;
+
+        public async void Execute(object parameter) {
+            this.IsExecuting = true;
+            try {
+                await this.ExecuteAsync(parameter);
+            }
+            catch (Exception exception) {
+                this._onException?.Invoke(exception);              
+            }    
+            this.IsExecuting = false;
+        }
+
+        protected abstract Task ExecuteAsync(object parameter);
+    }
+}
