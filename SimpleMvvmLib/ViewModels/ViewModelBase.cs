c0333e42a6e13aa8ff4ac84c83a5602c2035bba4
3k-dome <doenerfresser3k@gmail.com>
2022-08-04 19:38:57 +0200

    clean up

--- SimpleMvvmLib/ViewModels/ViewModelBase.cs
+++ SimpleMvvmLib/ViewModels/ViewModelBase.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleMvvmLib.ViewModels {
+
     public class ViewModelBase : INotifyPropertyChanged {
-        
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
